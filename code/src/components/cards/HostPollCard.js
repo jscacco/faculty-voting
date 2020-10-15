25b1757d2172518e24b728b614fbf4341329ef48
Connor Cates <35548067+ccates83@users.noreply.github.com>
2020-10-15 18:15:09 -0400

    added multi poll functionality to host poll

--- code/src/components/cards/HostPollCard.js
+++ code/src/components/cards/HostPollCard.js
@@ -70,23 +70,24 @@ const HostPollCard = ( props ) => {
   )
 
   const _renderOptionGroup = () => {
+    console.log(pollData.type)
     var optionComponents = pollData.options.map(optionData => {
+      var iconType = pollData.type === 'multiple' ? 'checkbox' : 'bubble';
+      console.log(iconType)
       return optionData.optionType === 'text' ?
-            <VotingOption medium fontColor={Colors.LightBlue}>
-              <TextOption>
+            <VotingOption medium fontColor={Colors.LightBlue} type={iconType}>
+              <Option >
                 {optionData.value}
-              </TextOption>
+              </Option>
             </VotingOption> :
-            <VotingOption medium>
-              <InputOption>
-                {optionData.value}
-              </InputOption>
+            <VotingOption medium fontColor={Colors.LightBlue} type={'iconType'}>
+              <InputOption />
             </VotingOption>;
     });
 
     return (
       <OptionGroupWrapper>
-        <OptionGroup onSelect={onOptionChange} selectedOptions={selectedOptions}>
+        <OptionGroup type={pollData.type} onSelect={onOptionChange} selectedOptions={selectedOptions}>
           {optionComponents}
         </OptionGroup>
       </OptionGroupWrapper>
