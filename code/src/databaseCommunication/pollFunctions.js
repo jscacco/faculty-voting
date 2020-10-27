b1f8b5669d168e62a662396f5dfc464c87a59358
Chad Dubin <54780293+cdubin33@users.noreply.github.com>
2020-10-26 21:00:23 -0400

    Tried to fix fetchAgenda

--- code/src/databaseCommunication/pollFunctions.js
+++ code/src/databaseCommunication/pollFunctions.js
@@ -57,7 +57,7 @@ const fetchPollData = async (host_id, room_id, poll_id) => {
           };
 
           poll.options[option_id] = opt;
-          console.log(poll.options);
+          //console.log(poll.options);
         }
 
         // const optionRef = firestore
@@ -81,7 +81,7 @@ const fetchPollData = async (host_id, room_id, poll_id) => {
         //     });
         // });
 
-        console.log(poll.options)
+        //console.log(poll.options)
 
         // Check the hash to make sure it's good
         if (!compareHashes(poll, docData['pollHash'])) {
@@ -96,15 +96,70 @@ const fetchPollData = async (host_id, room_id, poll_id) => {
     }
 }
 
+const fetchAgenda2 = async (host_id, room_id) => {
+    try {
+        let agenda = { polls: {}, order: {} };
+	    let fetchedHash = "";
+
+        await firestore.collection(host_id).doc(room_id).get().then(roomSnap => {
+            agenda['title'] = roomSnap.data()['title'];
+            agenda['status'] = roomSnap.data()['status'];
+	        fetchedHash = roomSnap.data()['agendaHash'];
+        });
+
+        const collect = firestore
+                            .collection(host_id)
+                            .doc(room_id)
+                            .collection('polls');
+
+        await collect.get().then(snap => {
+            //console.log(snap)
+            snap.forEach(async function (pollSnap) {
+                if(pollSnap.id != 'order') {
+                    /*var poll = { title: '', status: '', id: '' };
+                    //console.log(doc)
+                    poll['id'] = pollSnap.id;
+                    poll['status'] = pollSnap.data()['status'];
+                    poll['title'] = pollSnap.data()['title'];*/
+                    let poll = await fetchPollData(host_id, room_id, pollSnap.id);
+                    delete poll.userInputOption;
+                    delete poll.type;
+                    delete poll.showResults;
+                    delete poll.optionsOrder;
+                    delete poll.options;
+                    delete poll.description;
+
+                    console.log(poll);
+
+                    agenda['polls'][pollSnap.id] = poll;
+                }
+                else {
+                    agenda['order'] = pollSnap.data();
+
+                    //console.log(agenda['order'])
+                    /*agenda['order']['pending'] = pollSnap.data()['pending'];
+                    agenda['order']['open'] = pollSnap.data()['open'];
+                    agenda['order']['closed'] = pollSnap.data()['closed'];*/
+                }
+                });
+        });
+        console.log(agenda)
+        return agenda;
+    } catch (error) {
+        console.log("oh shit")
+        throw error;
+    }
+}
+
 const fetchAgenda = async (host_id, room_id) => {
     try {
         let agenda = { polls: {}, order: {} };
-	let fetchedHash = "";
+	    let fetchedHash = "";
 
         await firestore.collection(host_id).doc(room_id).get().then(roomSnap => {
-            agenda['title'] = roomSnap.data()['roomTitle'];
+            agenda['title'] = roomSnap.data()['title'];
             agenda['status'] = roomSnap.data()['status'];
-	    fetchedHash = roomSnap.data()['agendaHash'];
+	        fetchedHash = roomSnap.data()['agendaHash'];
         });
 
         const collect = firestore
@@ -115,23 +170,28 @@ const fetchAgenda = async (host_id, room_id) => {
         await collect.get().then(snap => {
             //console.log(snap)
             snap.forEach(function (pollSnap) {
-            if(pollSnap.id != 'order') {
-                var poll = { title: '', status: '', id: '' };
-                //console.log(doc)
-                poll['id'] = pollSnap.id;
-                poll['status'] = pollSnap.data()['status'];
-                poll['title'] = pollSnap.data()['title'];
-                agenda['polls'][pollSnap.id] = poll;
-            }
-            else {
-                agenda['order']['pending'] = pollSnap.data()['pending'];
-                agenda['order']['open'] = pollSnap.data()['open'];
-                agenda['order']['closed'] = pollSnap.data()['closed'];
-            }
+                if(pollSnap.id != 'order') {
+                    var poll = { title: '', status: '', id: '' };
+                    //console.log(doc)
+                    poll['id'] = pollSnap.id;
+                    poll['status'] = pollSnap.data()['status'];
+                    poll['title'] = pollSnap.data()['title'];
+                    agenda['polls'][pollSnap.id] = poll;
+                }
+                else {
+                    agenda['order']['pending'] = pollSnap.data()['pending'];
+                    agenda['order']['open'] = pollSnap.data()['open'];
+                    agenda['order']['closed'] = pollSnap.data()['closed'];
+                }
             });
             //return res.status(200).send(agenda);
         });
-      return agenda;
+        // console.log('current')
+        // console.log(agenda)
+        // console.log('new')
+        // await fetchAgenda2(host_id, room_id);
+
+        return agenda;
     } catch (error) {
         throw error;
     }
@@ -272,11 +332,11 @@ const getPollResults = async (host_id, room_id, poll_id) => {
         }
 
         return {
-        title: poll.title,
-        description: poll.description,
-        optionsOrder: poll.optionsOrder,
-        options: {...options},
-        results: {...results}
+            title: poll.title,
+            description: poll.description,
+            optionsOrder: poll.optionsOrder,
+            options: {...options},
+            results: {...results}
         }
     } catch (error) {
         throw error;
