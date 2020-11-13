40b7aba17400256fb76e2d2dfdd5eb5b258b17d2
Chad Dubin <54780293+cdubin33@users.noreply.github.com>
2020-11-13 14:48:33 -0500

    think voting is fixed

---
+++ code/build/precache-manifest.db8cece1fda33afd8db1875a3626c74d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e735a7a3acd742b8458fde64630c7902",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22bb7607bdd4828ae183",
+    "url": "/static/js/2.86453273.chunk.js"
+  },
+  {
+    "revision": "3a858bf484e2d304e9ca92d796eb3fcc",
+    "url": "/static/js/2.86453273.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f598ea4aed0423f3fa60",
+    "url": "/static/js/main.5d5502b6.chunk.js"
+  },
+  {
+    "revision": "e305a6a01887803ea7b7",
+    "url": "/static/js/runtime-main.098bc08b.js"
+  },
+  {
+    "revision": "41be865cba17caf2060f99aa0e5205e2",
+    "url": "/static/media/logo.41be865c.png"
+  }
+]);
